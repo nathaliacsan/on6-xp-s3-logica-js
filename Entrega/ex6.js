72442e5f6cc808efb0eb3e5363742c37f4a83415
Nathália Santos <nathaliacsan@gmail.com>
2020-08-26 21:13:32 -0300

    Subindo o exercício 6 com correção

--- Entrega/ex6.js
+++ Entrega/ex6.js
@@ -19,7 +19,7 @@
 
 function tabelaPrecos() {
     for (let i = 1; i <= 50; i++) {
-        console.log(` ${i} - R$ ${1.99 * i}`)
+        console.log(` ${i} - R$ ${(1.99 * i).toFixed(2)}`)
     }
 
 }
